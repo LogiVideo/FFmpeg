6db6310d92b92b47500cee2796b133f6aa841198
Andy Griffin <agriffin@logitech.com>
2018-10-10 16:09:01 -0700

    expose internal functions required by ISOMediaWriter via the ver file

--- libavformat/libavformat.v
+++ libavformat/libavformat.v
@@ -1,5 +1,17 @@
 LIBAVFORMAT_$MAJOR {
         global: av*;
+                #LOGI these are for ISOMediaWriter
+                mov_write_header;
+                mov_write_packet;
+                ff_data_to_hex;
+                mov_set_sequence;
+                mov_get_track_duration;
+                mov_write_trailer;
+                mov_get_flags;
+                ff_avc_find_startcode;
+                mov_get_last_track_last_cluster_pts;
+                mov_set_flags;
+                mov_set_track_duration;
                 #FIXME those are for ffserver
                 ff_inet_aton;
                 ff_socket_nonblock;
